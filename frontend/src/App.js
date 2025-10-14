ae79087cbecb789e1dccf5d5c26b8642210e8df4
Vonamto <touatgeneral@gmail.com>
2025-10-14 16:46:33 +0100

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
+      <h1>Machinery Maintenance App</h1>
+      <p>English / العربية toggle coming soon 🇬🇧 🇸🇦</p>
+    </div>
+  );
+}
+
+export default App;
