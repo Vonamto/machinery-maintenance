079ff447f5121221f34a8a117669a88331030c1c
Vonamto <touatgeneral@gmail.com>
2025-10-23 12:10:21 +0100

    Update main.jsx auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
+// frontend/src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// NEW: AuthProvider
+import { AuthProvider } from "./context/AuthContext";
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
