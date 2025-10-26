0c34b3cac6b60bff6804a7f5c43727709bd09bdd
Vonamto <touatgeneral@gmail.com>
2025-10-26 13:24:30 +0100

    Update index.jsx test

--- frontend/src/pages/Requests/Parts/index.jsx
+++ frontend/src/pages/Requests/Parts/index.jsx
@@ -1,4 +1,4 @@
-// frontend/src/pages/Requests/Parts/index.jsx
+ // frontend/src/pages/Requests/Parts/index.jsx
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Card, CardContent } from "@/components/ui/card";
