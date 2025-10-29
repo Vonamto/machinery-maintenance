6893fb7409e8215414f2fb5510fc34ed195bce63
Vonamto <touatgeneral@gmail.com>
2025-10-29 18:01:20 +0100

    Delete frontend/src/pages/Users.jsx

--- frontend/src/pages/Users.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Users() {
-  return (
-    <div className="flex items-center justify-center h-screen text-gray-700 text-xl">
-      👥 Users Management — coming soon
-    </div>
-  );
-}
